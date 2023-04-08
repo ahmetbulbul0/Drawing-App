919f82cdf025bf95675aa52b671367437a13f38f
ahmetbulbul0 <ahmetbulbul@protonmail.com>
2023-04-08 05:34:56 +0300

    drawing board f

---
+++ script.js
@@ -0,0 +1,48 @@
+const canvas = document.querySelector("canvas"),
+    toolBtns = document.querySelectorAll(".tool"),
+    ctx = canvas.getContext("2d");
+
+let prevMouseX, prevMouseY,
+    isDrawing = false,
+    selectedTool = "brush",
+    brushWidth = 5;
+
+window.addEventListener("load", () => {
+    canvas.width = canvas.offsetWidth;
+    canvas.height = canvas.offsetHeight;
+});
+
+const drawRect = (e) => {
+    ctx.strokeRect(e.offsetX, e.offsetY, prevMouseX - e.offsetX, prevMouseY - e.offsetY);
+}
+
+const startDraw = (e) => {
+    isDrawing = true;
+    prevMouseX = e.offsetX;
+    prevMouseY = e.offsetY;
+    ctx.beginPath();
+    ctx.lineWidth = brushWidth;
+}
+
+const drawing = (e) => {
+    if (!isDrawing) return;
+    if (selectedTool === "brush") {
+        ctx.lineTo(e.offsetX, e.offsetY);
+        ctx.stroke();
+    } else if (selectedTool === "rectangle") {
+        drawRect(e);
+    }
+}
+
+toolBtns.forEach(btn => {
+    btn.addEventListener("click", () => {
+        document.querySelector(".options .active").classList.remove("active");
+        btn.classList.add("active");
+        selectedTool = btn.id;
+        console.log(selectedTool);
+    })
+});
+
+canvas.addEventListener("mousedown", startDraw);
+canvas.addEventListener("mousemove", drawing);
+canvas.addEventListener("mouseup", () => isDrawing = false);
